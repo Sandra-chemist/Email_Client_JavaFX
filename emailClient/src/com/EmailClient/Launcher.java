8f6cc88ade8ecb3dd24927b5f7d86df927926e1c
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-11-29 08:56:51 +0100

    built the login window

---
+++ emailClient/src/com/EmailClient/Launcher.java
@@ -0,0 +1,26 @@
+package com.EmailClient;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent parent = FXMLLoader.load(getClass().getResource("view/LoginWindow.fxml"));
+
+        Scene scene = new Scene(parent, 433, 339);
+        stage.setScene(scene);
+
+        stage.show();
+
+    }
+}
