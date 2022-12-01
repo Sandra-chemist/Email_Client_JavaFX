06d5299f36259819114531bd34ebc8d70ad11566
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-12-01 14:50:32 +0100

    added messages to folder

--- emailClient/src/main/controller/services/FetchFoldersService.java
+++ emailClient/src/main/controller/services/FetchFoldersService.java
@@ -48,7 +48,6 @@ public class FetchFoldersService extends Service<Void> {
         }
 
     }
-
     private void fetchMessagesOnFolder(Folder folder, EmailTreeItem<String> emailTreeItem) {
         Service fetchMessagesService = new Service() {
             @Override
@@ -60,7 +59,7 @@ public class FetchFoldersService extends Service<Void> {
                             folder.open(Folder.READ_WRITE);
                             int folderSize = folder.getMessageCount();
                             for (int i = folderSize; i > 0; i--) {
-                                System.out.println(folder.getMessage(i).getSubject());
+                                emailTreeItem.addEmail(folder.getMessage(i));
                             }
                         }
                         return null;
