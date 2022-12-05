5263001b6b22275fcc53504b71f55cdb0044c966
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-12-05 11:28:00 +0100

    added create the message in EmailSendingResult

--- emailClient/src/main/controller/services/EmailSenderService.java
+++ emailClient/src/main/controller/services/EmailSenderService.java
@@ -5,6 +5,10 @@ import javafx.concurrent.Task;
 import main.controller.EmailSendingResult;
 import main.model.EmailAccount;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 public class EmailSenderService extends Service<EmailSendingResult> {
 
     private EmailAccount emailAccount;
@@ -23,7 +27,22 @@ public class EmailSenderService extends Service<EmailSendingResult> {
     protected Task<EmailSendingResult> createTask() {
         return new Task<EmailSendingResult>() {
             @Override
-            protected EmailSendingResult call() throws Exception {
+            protected EmailSendingResult call() {
+                try {
+                    // create the message
+                    MimeMessage mimeMessage = new MimeMessage(emailAccount.getSession());
+                    mimeMessage.setFrom(emailAccount.getAddress());
+                    mimeMessage.addRecipients(Message.RecipientType.TO, recipient);
+                    mimeMessage.setSubject(subject);
+
+
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                    return EmailSendingResult.FAILED_BY_PROVIDER;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return EmailSendingResult.FAILED_BY_UNEXPECTED_ERROR;
+                }
                 return null;
             }
         };
